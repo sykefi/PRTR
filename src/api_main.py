4031b571656a4c608ee99cef6d6140409beee627
Joose Helle <joose.helle@gmail.com>
2021-07-20 15:11:44 +0300

    add some initial query params

--- src/api_main.py
+++ src/api_main.py
@@ -1,18 +1,18 @@
-from api.models import Facility
-from typing import List
+from api.models import Facility, facility_csv_dict_2_facility
 from api.conf import conf
-import api.models as models
 import csv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from typing import List
 
 
 facilities: List[Facility] = [
-    models.facility_csv_dict_2_facility(d) for d
+    facility_csv_dict_2_facility(d) for d
     in csv.DictReader(open(conf.facilities_csv_fp), delimiter=';')
 ]
 
 
 root_path = f'/api/{conf.api_version}'
+
 
 app = FastAPI(
     title=conf.api_title,
@@ -32,6 +32,17 @@ def read_root():
     }
 
 
-@app.get(f'{root_path}/facilities', response_model=List[Facility])
-def get_facilities():
-    return facilities[:5]
+@app.get(
+    f'{root_path}/facilities',
+    response_model=List[Facility],
+    status_code=200
+)
+def read_facilities(facility_id: str = None, skip: int = 0, limit: int = 10):
+    if not facility_id:
+        return facilities[skip:skip + limit]
+
+    match = [f for f in facilities if f.facilityInspireId == facility_id]
+    if not match:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, 'Facility not found')
+
+    return match
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from api_main import app
+from api_main import app, root_path
 from api.conf import conf
 
 
@@ -7,10 +7,17 @@ client = TestClient(app)
 
 
 def test_root_response():
-    response = client.get("/")
+    response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {
         'title': conf.api_title,
         'description': conf.api_description,
         'documentation': app.docs_url,
     }
+
+
+def test_get_facilities():
+    response = client.get(f'{root_path}/facilities')
+    assert response.status_code == 200
+    body = response.json()
+    assert len(body) > 1
