010b1b242825b3cc75045d07fe7b886345cf6a7e
Joose Helle <joose.helle@gmail.com>
2021-07-22 09:55:32 +0300

    Add workflow for building latest image for tests

--- build-image.sh
+++ build-image.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -ex
 
-USER='hellej'
+USER='prtrdev.azurecr.io'
 DOCKER_IMAGE=${USER}/fin-prtr
 
 docker build -t ${DOCKER_IMAGE}:latest .
 
-docker push ${DOCKER_IMAGE}:latest
+docker login prtrdev.azurecr.io
+docker push prtrdev.azurecr.io/fin-prtr:latest
