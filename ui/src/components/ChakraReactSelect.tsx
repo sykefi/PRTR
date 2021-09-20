713909f0184eb5d14de4395cd412cd7fc05df705
Joose Helle <joose.helle@gmail.com>
2021-09-20 10:51:03 +0300

    Add filter input for facility type WIP

---
+++ ui/src/components/ChakraReactSelect.tsx
@@ -0,0 +1,245 @@
+//https://gist.github.com/csandman/c687a9fb4275112f281ab9a5701457e4#gistcomment-3758944
+//https://dev.azure.com/sykefi/SeSu/_git/sesu-frontend?path=/src/features/chakraReactSelect
+
+import Select, {
+  components as selectComponents,
+  GroupTypeBase,
+  OptionTypeBase,
+  Props as SelectProps,
+  SelectComponentsConfig,
+  StylesConfig,
+  Theme
+} from 'react-select'
+import {
+  chakra,
+  Flex,
+  Tag,
+  TagCloseButton,
+  TagLabel,
+  Divider,
+  CloseButton,
+  Center,
+  Portal,
+  StylesProvider,
+  useMultiStyleConfig,
+  useStyles,
+  useTheme,
+  useColorModeValue,
+  Icon,
+  RecursiveCSSObject,
+  CSSWithMultiValues
+} from '@chakra-ui/react'
+import { ChevronDownIcon } from '@chakra-ui/icons'
+
+export interface IMultiSelectItem {
+  label: string
+  value: string
+}
+interface ItemProps extends CSSWithMultiValues {
+  _disabled: CSSWithMultiValues
+  _focus: CSSWithMultiValues
+}
+
+const chakraStyles: SelectProps['styles'] = {
+  input: provided => ({
+    ...provided,
+    color: 'inherit',
+    lineHeight: 1
+  }),
+  menu: provided => ({
+    ...provided,
+    boxShadow: 'none',
+    marginTop: '5px'
+  }),
+  valueContainer: provided => ({
+    ...provided,
+    padding: '0.125rem 1rem',
+    background: 'white'
+  }),
+  indicatorsContainer: provided => ({
+    ...provided,
+    backgroundColor: 'white'
+  })
+}
+
+const chakraComponents: SelectProps['components'] = {
+  Control: ({ children, innerRef, innerProps, isDisabled, isFocused }) => {
+    const inputStyles = useMultiStyleConfig('Input', {})
+    return (
+      <StylesProvider value={inputStyles}>
+        <Flex
+          ref={innerRef}
+          sx={{
+            ...inputStyles.field,
+            p: 0,
+            overflow: 'hidden',
+            h: 'auto',
+            minH: 10
+          }}
+          {...innerProps}
+          {...(isFocused && { 'data-focus': true })}
+          {...(isDisabled && { disabled: true })}>
+          {children}
+        </Flex>
+      </StylesProvider>
+    )
+  },
+  MultiValueContainer: ({
+    children,
+    innerRef,
+    innerProps,
+    data: { isFixed }
+  }) => (
+    <Tag
+      ref={innerRef}
+      {...innerProps}
+      m="0.125rem"
+      variant={isFixed ? 'solid' : 'subtle'}>
+      {children}
+    </Tag>
+  ),
+  MultiValueLabel: ({ children, innerRef, innerProps }) => (
+    <TagLabel ref={innerRef} {...innerProps}>
+      {children}
+    </TagLabel>
+  ),
+  MultiValueRemove: ({ children, innerRef, innerProps, data: { isFixed } }) => {
+    if (isFixed) {
+      return null
+    }
+
+    return (
+      <TagCloseButton ref={innerRef} {...innerProps}>
+        {children}
+      </TagCloseButton>
+    )
+  },
+  IndicatorSeparator: ({ innerProps }) => (
+    <Divider {...innerProps} orientation="vertical" opacity="1" />
+  ),
+  ClearIndicator: ({ innerProps }) => (
+    <CloseButton {...innerProps} size="sm" mx={2} />
+  ),
+  DropdownIndicator: ({ innerProps }) => {
+    const { addon } = useStyles()
+
+    return (
+      <Center
+        {...innerProps}
+        sx={{
+          ...addon,
+          px: '2', // Sesu addition
+          h: '100%',
+          borderRadius: 0,
+          borderWidth: 0,
+          cursor: 'pointer',
+          background: 'white'
+        }}>
+        <Icon as={ChevronDownIcon} h={5} w={5} />
+      </Center>
+    )
+  },
+  MenuPortal: ({ children }) => <Portal>{children}</Portal>,
+  Menu: ({ children, ...props }) => {
+    const menuStyles = useMultiStyleConfig('Menu', {})
+    return (
+      <selectComponents.Menu {...props}>
+        <StylesProvider value={menuStyles}>{children}</StylesProvider>
+      </selectComponents.Menu>
+    )
+  },
+  MenuList: ({ innerRef, children, maxHeight }) => {
+    const { list } = useStyles()
+    return (
+      <chakra.div
+        sx={{
+          ...list,
+          maxH: `${maxHeight}px`,
+          overflowY: 'auto'
+        }}
+        ref={innerRef}>
+        {children}
+      </chakra.div>
+    )
+  },
+  GroupHeading: ({ innerProps, children }) => {
+    const { groupTitle } = useStyles()
+    return (
+      <chakra.div sx={groupTitle} {...innerProps}>
+        {children}
+      </chakra.div>
+    )
+  },
+  Option: ({ innerRef, innerProps, children, isFocused, isDisabled }) => {
+    const { item } = useStyles()
+    return (
+      <chakra.div
+        role="button"
+        sx={{
+          ...item,
+          w: '100%',
+          textAlign: 'left',
+          bg: isFocused
+            ? (item as RecursiveCSSObject<ItemProps>)._focus.bg
+            : 'transparent',
+          ...(isDisabled && (item as RecursiveCSSObject<ItemProps>)._disabled)
+        }}
+        ref={innerRef}
+        {...innerProps}
+        {...(isDisabled && { disabled: true })}>
+        {children}
+      </chakra.div>
+    )
+  }
+}
+
+export function ChakraSelect<
+  OptionType extends OptionTypeBase = { label: string; value: string },
+  IsMulti extends boolean = false,
+  GroupType extends GroupTypeBase<OptionType> = GroupTypeBase<OptionType>
+>({
+  name = '',
+  styles = {},
+  components = {},
+  theme,
+  ...props
+}: SelectProps<OptionType, IsMulti, GroupType>) {
+  const chakraTheme = useTheme()
+  const placeholderColor = useColorModeValue(
+    chakraTheme.colors.gray[400],
+    chakraTheme.colors.whiteAlpha[400]
+  )
+
+  return (
+    <Select
+      name={name}
+      components={{
+        ...(chakraComponents as SelectComponentsConfig<
+          OptionType,
+          IsMulti,
+          GroupType
+        >),
+        ...components
+      }}
+      styles={{
+        ...(chakraStyles as StylesConfig<OptionType, IsMulti, GroupType>),
+        ...styles
+      }}
+      theme={baseTheme => ({
+        ...baseTheme,
+        borderRadius: chakraTheme.radii.md,
+        colors: {
+          ...baseTheme.colors,
+          neutral50: placeholderColor, // placeholder text color
+          neutral40: placeholderColor, // noOptionsMessage color
+          ...(theme as Theme)?.colors
+        },
+        spacing: {
+          ...baseTheme.spacing,
+          ...(theme as Theme)?.spacing
+        }
+      })}
+      {...props}
+    />
+  )
+}
--- ui/src/components/FacilityList/FacilitySearchPanel.tsx
+++ ui/src/components/FacilityList/FacilitySearchPanel.tsx
@@ -1,12 +1,19 @@
-import { Button } from '@chakra-ui/button'
-import { Input } from '@chakra-ui/input'
-import { Flex } from '@chakra-ui/layout'
-import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
+import { Button } from '@chakra-ui/button'
+import { FormControl, FormLabel } from '@chakra-ui/form-control'
+import { Input } from '@chakra-ui/input'
+import { Box, Flex } from '@chakra-ui/layout'
+import { useTranslation } from 'react-i18next'
+import { ChakraSelect } from '../ChakraReactSelect'
 
 const Form = styled.form`
   max-width: 100%;
 `
+
+export const colourOptions = [
+  { value: '2(b)', label: 'Rauta- ja terästeollisuus' },
+  { value: '5(f)', label: 'Yhdyskuntajätevesien käsittelylaitokset' }
+]
 
 export const FacilitySearchPanel = ({
   searchTerm,
@@ -21,28 +28,44 @@ export const FacilitySearchPanel = ({
 
   return (
     <Form onSubmit={handleSubmit} data-cy="facility-search-panel">
-      <Flex marginTop={1.0} marginBottom={2.0} flexWrap="wrap" justify="center">
-        <Input
-          data-cy="facility-search-term"
-          type="text"
-          bgColor="white"
-          width={438}
-          minWidth={200}
-          maxWidth="100%"
-          marginY={1.0}
-          value={searchTerm}
-          onChange={e => setSearchTerm(e.target.value)}
-          placeholder={t('common.searchTerm')}
-        />
-        <Button
-          data-cy="search-facilities-btn"
-          type="submit"
-          marginY={1.0}
-          marginLeft={1.0}
-          disabled={searchTerm === ''}
-          colorScheme="green">
-          {t('common.search')}
-        </Button>
+      <Flex display="flex" flexWrap="wrap" justify="center">
+        <FormControl
+          marginTop={1.0}
+          marginBottom={2.0}
+          width={550}
+          minWidth={250}>
+          <Box marginY={1.0}>
+            <ChakraSelect
+              isClearable
+              closeMenuOnSelect
+              name="colors"
+              options={colourOptions}
+              borderColor="blue"
+              placeholder="Valitse toimiala"
+              onChange={e => console.log(e)}
+            />
+          </Box>
+          <Input
+            data-cy="facility-search-term"
+            type="text"
+            bgColor="white"
+            minWidth={200}
+            maxWidth="100%"
+            marginY={1.0}
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+            placeholder={t('common.searchTerm')}
+          />
+          <Button
+            data-cy="search-facilities-btn"
+            type="submit"
+            marginY={1.0}
+            marginLeft={1.0}
+            disabled={searchTerm === ''}
+            colorScheme="green">
+            {t('common.search')}
+          </Button>
+        </FormControl>
       </Flex>
     </Form>
   )
