e76d01ee817e7de90d6fbaed61cc30203aeac242
Joose Helle <joose.helle@gmail.com>
2021-09-23 10:57:55 +0300

    Add error handling to facility page

---
+++ ui/src/components/FacilityPage/FacilityBasicInfo.tsx
@@ -0,0 +1,104 @@
+import { Button } from '@chakra-ui/button'
+import { Box } from '@chakra-ui/layout'
+import { useTranslation } from 'react-i18next'
+import { useHistory } from 'react-router'
+import { Facility } from '../../api/models/Facility'
+import { LoadAnimation } from '../LoadAnimation/LoadAnimation'
+
+const InfoPropRow = ({
+  label,
+  value
+}: {
+  label: string
+  value: string | null
+}) => {
+  return (
+    <Box fontSize="smaller" marginY={2}>
+      <Box fontWeight="semibold">{label}</Box>
+      <Box marginTop={0.5} color="blackAlpha.800">
+        {value || '-'}
+      </Box>
+    </Box>
+  )
+}
+
+export const FacilityBasicInfo = ({
+  facility,
+  loading,
+  error
+}: {
+  facility: Facility | null
+  loading: boolean
+  error: boolean
+}) => {
+  const history = useHistory()
+  const { t } = useTranslation(['translation', 'mainActivityCodeDesc'])
+
+  return (
+    <Box
+      width={450}
+      minWidth={250}
+      maxWidth="100%"
+      m={2}
+      paddingX={5}
+      paddingY={2}
+      height="max-content"
+      maxHeight={600}
+      overflowY="auto"
+      background="white"
+      borderRadius="md"
+      boxShadow="sm">
+      <Box as="h1" fontWeight="semibold" marginY={2}>
+        {t('translation:common.basicInfo')}
+      </Box>
+      {facility && (
+        <>
+          <InfoPropRow
+            label={t('translation:facilities.facilityTypeCode')}
+            value={
+              facility.mainActivityCode +
+              (t(`mainActivityCodeDesc:${facility.mainActivityCode}`)
+                ? ' - ' + t(`mainActivityCodeDesc:${facility.mainActivityCode}`)
+                : '')
+            }
+          />
+          <InfoPropRow
+            label={t('translation:common.municipality')}
+            value={facility.city}
+          />
+          <InfoPropRow
+            label={t('translation:common.streetAddress')}
+            value={`${facility.streetName} ${facility.buildingNumber || ''}`}
+          />
+          <InfoPropRow
+            label={t('translation:common.telephoneNumber')}
+            value={facility.telephoneNo || ''}
+          />
+        </>
+      )}
+      {loading && (
+        <Box p={2} data-cy="facility-info-load-animation">
+          <LoadAnimation sizePx={30} />
+        </Box>
+      )}
+      {error && (
+        <>
+          <Box marginTop={2.0}>
+            <Box fontSize="smaller">
+              {t('translation:facilities.couldNotFindFacilityWithId')}
+            </Box>
+            <Box>
+              <Button
+                marginY={2.0}
+                size="sm"
+                colorScheme="blue"
+                onClick={() => history.push('/')}>
+                {t('translation:common.goBack')}
+              </Button>
+            </Box>
+          </Box>
+        </>
+      )}
+    </Box>
+  )
+}
--- ui/src/components/FacilityPage/FacilityPage.tsx
+++ ui/src/components/FacilityPage/FacilityPage.tsx
@@ -1,42 +1,23 @@
-import { Button } from '@chakra-ui/button'
 import { Box, Flex, Heading } from '@chakra-ui/layout'
-import { useHistory } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router-dom'
 import { getFacility } from '../../api'
 import { Facility } from '../../api/models/Facility'
-import { LoadAnimation } from '../LoadAnimation/LoadAnimation'
 import { OlMap } from '../OlMap'
 import { FacilityReleaseInfo } from './FacilityReleaseInfo'
+import { FacilityBasicInfo } from './FacilityBasicInfo'
 
 type FacilityParams = {
   facilityId: string
-}
-
-const InfoPropRow = ({
-  label,
-  value
-}: {
-  label: string
-  value: string | undefined
-}) => {
-  return (
-    <Box fontSize="smaller" marginY={2}>
-      <Box fontWeight="semibold">{label}</Box>
-      <Box marginTop={0.5} color="blackAlpha.800">
-        {value || '-'}
-      </Box>
-    </Box>
-  )
 }
 
 export const FacilityPage = () => {
   const [infoState, setInfoState] = useState<
     'initial' | 'loading' | 'error' | 'done'
   >('initial')
+  const loading = infoState === 'initial' || infoState === 'loading'
   const { t } = useTranslation(['translation', 'mainActivityCodeDesc'])
-  const history = useHistory()
   const [facility, setFacility] = useState<Facility | null>(null)
   const { facilityId } = useParams<FacilityParams>()
 
@@ -65,83 +46,28 @@ export const FacilityPage = () => {
     }
   }, [facilityId])
 
-  if (infoState === 'loading') {
-    return (
-      <Box p={4} data-cy="facility-info-load-animation">
-        <LoadAnimation sizePx={30} />
-      </Box>
-    )
-  }
-
-  if (infoState === 'error') {
-    return (
-      <Box margin={1.0} marginY={2.0} fontWeight="bold">
-        <Box>{t('translation:facilities.couldNotFindFacilityWithId')}</Box>
-        <Box>
-          <Button
-            marginY={2.0}
-            size="sm"
-            colorScheme="blue"
-            onClick={() => history.push('/')}>
-            {t('translation:common.goBack')}
-          </Button>
-        </Box>
-      </Box>
-    )
-  }
-
-  if (!facility) return null
-
   return (
     <>
       <Flex w="100%" justify="center">
         <Heading as="h3" size="md" marginBottom={3}>
-          {facility.nameOfFeature}
+          {facility?.nameOfFeature}
         </Heading>
+        {loading && (
+          <Box paddingBottom={2}>
+            {t('translation:common.loadingInformation')}
+          </Box>
+        )}
       </Flex>
       <Flex
         wrap="wrap"
         justify="center"
         maxWidth="100%"
         data-cy="facility-info-container">
-        <Box
-          width={450}
-          minWidth={250}
-          maxWidth="100%"
-          m={2}
-          paddingX={5}
-          paddingY={2}
-          height="max-content"
-          maxHeight={600}
-          overflowY="auto"
-          background="white"
-          borderRadius="md"
-          boxShadow="sm">
-          <Box as="h1" fontWeight="semibold" marginY={2}>
-            {t('translation:common.basicInfo')}
-          </Box>
-          <InfoPropRow
-            label={t('translation:facilities.facilityTypeCode')}
-            value={
-              facility.mainActivityCode +
-              (t(`mainActivityCodeDesc:${facility.mainActivityCode}`)
-                ? ' - ' + t(`mainActivityCodeDesc:${facility.mainActivityCode}`)
-                : '')
-            }
-          />
-          <InfoPropRow
-            label={t('translation:common.municipality')}
-            value={facility.city}
-          />
-          <InfoPropRow
-            label={t('translation:common.streetAddress')}
-            value={`${facility.streetName} ${facility.buildingNumber || ''}`}
-          />
-          <InfoPropRow
-            label={t('translation:common.telephoneNumber')}
-            value={facility.telephoneNo || ''}
-          />
-        </Box>
+        <FacilityBasicInfo
+          loading={loading}
+          error={infoState === 'error'}
+          facility={facility}
+        />
         <FacilityReleaseInfo facilityId={facilityId} />
         <OlMap
           margin={2}
