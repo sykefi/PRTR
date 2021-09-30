c36528e7070ba60595e412a1ae3f01a9997f0b58
Joose Helle <joose.helle@gmail.com>
2021-09-30 15:24:05 +0300

    Add CAS numbers to pollutant labels

--- ui/src/hooks/useGetPollutantLabel.tsx
+++ ui/src/hooks/useGetPollutantLabel.tsx
@@ -2,10 +2,19 @@ import { useTranslation } from 'react-i18next'
 import { PollutantCode } from '../api/models/PollutantCode'
 
 export const useGetPollutantLabel = () => {
-  const { t } = useTranslation(['pollutantName', 'pollutantAbbreviation'])
+  const { t } = useTranslation([
+    'pollutantName',
+    'pollutantAbbreviation',
+    'pollutantCasNumber'
+  ])
   return (pollutant: PollutantCode) => {
     const name = t(`pollutantName:${pollutant}`)
     const abbr = t(`pollutantAbbreviation:${pollutant}`)
-    return `${abbr}${abbr && name && ' - '} ${name}`
+    const cas = t(`pollutantCasNumber:${pollutant}`)
+    const brackets =
+      !abbr && !cas
+        ? ''
+        : ` (${abbr}${abbr && cas && ', '}${cas && 'CAS: ' + cas})`
+    return `${name}${brackets}`
   }
 }
