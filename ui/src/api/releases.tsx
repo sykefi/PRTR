2e71df60ad6d21784fb1c4b931d29484b809e92b
Joose Helle <joose.helle@gmail.com>
2021-09-30 12:46:24 +0300

    Refactor

--- ui/src/api/releases.tsx
+++ ui/src/api/releases.tsx
@@ -1,34 +1,10 @@
-import APIError from '../models/APIError'
 import { releasesResultLimit } from '../env'
 import { apiBasePath } from './conf'
-import { serializeQueryParams } from './utils'
+import { serializeQueryParams, fetchData } from './utils'
 import { ReleaseQueryParams } from './models/ReleaseQueryParams'
 import { PollutantRelease, withId } from './models/PollutantRelease'
 import { ReleasesResponse } from './models/ReleasesResponse'
 import { PRTRListResponse } from './models/PRTRListResponse'
-
-const getData = async <T,>(url: string, signal?: AbortSignal): Promise<T> => {
-  try {
-    const res = await fetch(url, { signal })
-    if (!res.ok) {
-      let errorBody
-      try {
-        errorBody = await res.json()
-      } catch {
-        errorBody = undefined
-      }
-      throw new Error(`Fetch failed: ${url} ->
-        url=${res.url}
-        status=${res.status}
-        statusText=${res.statusText}
-        body=${JSON.stringify(errorBody)}`)
-    }
-    const body = await res.json()
-    return (await body) as T
-  } catch (e) {
-    throw new APIError(e as Error)
-  }
-}
 
 export const getReleases = async (
   queryParams: ReleaseQueryParams,
@@ -42,7 +18,7 @@ export const getReleases = async (
     allQueryParams as Record<string, string | number>
   )
   const url = apiBasePath + '/releases?' + queryString
-  const body = await getData<ReleasesResponse>(url, signal)
+  const body = await fetchData<ReleasesResponse>(url, signal)
 
   return {
     ...body,
--- ui/src/api/utils.tsx
+++ ui/src/api/utils.tsx
@@ -1,4 +1,5 @@
 import { pickBy } from 'lodash'
+import APIError from '../models/APIError'
 
 export const serializeQueryParams = (obj: Record<string, string | number>) => {
   return Object.entries(pickBy(obj, v => v !== undefined))
@@ -8,4 +9,30 @@ export const serializeQueryParams = (obj: Record<string, string | number>) => {
       )
     }, [])
     .join('&')
+}
+
+export const fetchData = async <T,>(
+  url: string,
+  signal?: AbortSignal
+): Promise<T> => {
+  try {
+    const res = await fetch(url, { signal })
+    if (!res.ok) {
+      let errorBody
+      try {
+        errorBody = await res.json()
+      } catch {
+        errorBody = undefined
+      }
+      throw new Error(`Fetch failed: ${url} ->
+        status=${res.status}
+        statusText=${res.statusText}
+        url=${res.url}
+        body=${JSON.stringify(errorBody)}`)
+    }
+    const body = await res.json()
+    return (await body) as T
+  } catch (e) {
+    throw new APIError(e as Error)
+  }
 }
