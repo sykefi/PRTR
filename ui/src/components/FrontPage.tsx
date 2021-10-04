cd3f7da7ddb18eaec9010dcb98c1d5d9bb658381
Joose Helle <joose.helle@gmail.com>
2021-10-04 17:44:23 +0300

    Update front page with some initial content

--- ui/src/components/FrontPage.tsx
+++ ui/src/components/FrontPage.tsx
@@ -1,11 +1,36 @@
-import { Flex } from '@chakra-ui/layout'
-import { useTranslation } from 'react-i18next'
+import { Box, Flex, Heading, Link, Text } from '@chakra-ui/layout'
+import { Trans, useTranslation } from 'react-i18next'
 
 export const FrontPage = () => {
   const { t } = useTranslation()
+
   return (
-    <Flex p={6} fontWeight="semibold" justify="center">
-      {t('common.welcomeMessage')}
+    <Flex direction="column" align="center" p={3}>
+      <Box
+        p={6}
+        data-cy="front-page-content"
+        width="100%"
+        minWidth={250}
+        maxWidth={900}
+        background="white"
+        borderRadius="md"
+        boxShadow="md">
+        <Heading as="h3" fontSize="larger">
+          {t('common.welcomeMessage')}
+        </Heading>
+        <Text paddingTop={4} fontWeight="normal">
+          {t('textContent.p1')}
+        </Text>
+        <Text paddingTop={5} fontWeight="normal">
+          <Trans i18nKey="textContent.p2">
+            <Link
+              textDecoration="underline"
+              target="_blank"
+              href="https://www.eea.europa.eu/data-and-maps/data/industrial-reporting-under-the-industrial-3"
+              rel="noopener noreferrer external"></Link>
+          </Trans>
+        </Text>
+      </Box>
     </Flex>
   )
 }
