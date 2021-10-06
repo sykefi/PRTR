283fcf13bfbf187483acf2eeca412a917a89bfd2
Joose Helle <joose.helle@gmail.com>
2021-10-06 13:07:41 +0300

    Drop/merge duplicate facilities by name

--- api/data_import/utils.py
+++ api/data_import/utils.py
@@ -1,6 +1,7 @@
 from data_import.conf import conf
-from typing import Dict
+from typing import Dict, List, Union
 from pandas import DataFrame
+import numpy as np
 import pyproj
 from pyproj import CRS
 from shapely.geometry import Point
@@ -58,7 +59,10 @@ def clean_id(id_str: str) -> str:
     return clean_id
 
 
-def validate_ids(df: DataFrame, id_col='facilityId'):
+def ensure_unique_ids(df: DataFrame, id_col='facilityId'):
+    """Throws ValueError if duplicate IDs are found in facilities DataFrame.
+    """
+
     ids = list(df[id_col])
     if len(ids) != len(set(ids)):
         raise ValueError(
@@ -113,3 +117,169 @@ def print_unique_values_as_enum(df: DataFrame, col: str):
     for value in sorted([v for v in unique_values if v is not None]):
         print(f"    {value} = '{value}'")
     print('\n')
+
+
+def ensure_no_unlinked_releases(facilities: DataFrame, releases: DataFrame):
+    """Throws ValueError if some releases are found that are not linked to
+    facilities by facilityId.
+    """
+
+    facility_ids = list(facilities['facilityId'])
+    unlinked_releases = [
+        rd for rd
+        in releases.to_dict(orient='records')
+        if rd['facilityId'] not in facility_ids
+    ]
+    if unlinked_releases:
+        error = (
+            f'Found {len(unlinked_releases)} unlinked releases by facilityId',
+            ' (i.e. releases without facility in the facilities DataFrame).'
+        )
+        raise ValueError(error)
+
+
+def _facilities_can_be_merged(duplicates: List[dict]) -> bool:
+    """For a given set of possible duplicate facilities, returns a boolean
+    value indicating whether they can be merged. Currently only facilities
+    with main activity code 7* cannot be merged.
+    TODO: add additional logic here if needed.
+    """
+
+    has_main_activity_7 = [
+        d for d in duplicates
+        if '7' in d['mainActivityCode']
+    ]
+    if has_main_activity_7:
+        return False
+    return True
+
+
+def _get_facility_to_merge_to(duplicates: List[dict]) -> Union[dict, None]:
+    """For a given set of duplicate facilities, returns the facility that
+    the other ones should be merged to. Currently the latest facility that
+    has CompetentAuthorityEPRTRId is considered as the merge-to-facility.
+
+    Returns None if merge-to-facility cannot be determined.
+    """
+
+    duplicates_sorted = sorted(
+        duplicates,
+        key=lambda d: d['reportingYear'],
+        reverse=True
+    )
+    for d in duplicates_sorted:
+        # return first/latest facility that has CompetentAuthorityEPRTRId
+        # as the merge-to-facility
+        if not np.isnan(d['CompetentAuthorityEPRTRId']):
+            return d
+    return None
+
+
+def _get_merge_map_for_duplicates(
+    duplicates: List[dict]
+) -> Union[Dict[str, str], None]:
+    """For a given set of possibly duplicate facilities, returns a dictionary
+    representing merge actions (from facilityId to another facilityId) that
+    are needed. Returns None if merge-to-facility cannot be determined.
+    """
+
+    merge_to = _get_facility_to_merge_to(duplicates)
+    if not merge_to:
+        return None
+    return {
+        d['facilityId']: merge_to['facilityId']
+        for d in duplicates
+        if d['facilityId'] != merge_to['facilityId']
+    }
+
+
+def _get_id_to_id_merge_map_for_facilities(
+    facilities: DataFrame
+) -> Union[Dict[str, str], None]:
+    facility_dicts = facilities.to_dict(orient='records')
+    facility_dicts = sorted(
+        facility_dicts,
+        key=lambda d: d['nameOfFeature']
+    )
+
+    # collect list of facilities by name
+    facilities_by_name: Dict[str, List[dict]] = {
+        facility['nameOfFeature']: [
+            f for f in facility_dicts
+            if f['nameOfFeature'] == facility['nameOfFeature']
+        ]
+        for facility in facility_dicts
+    }
+
+    # keep list of facilities by name if there are more than one
+    # facility by the name
+    duplicates_by_name: Dict[str, List[dict]] = {
+        name: facilities
+        for name, facilities in facilities_by_name.items()
+        if len(facilities) > 1
+    }
+
+    merge_maps_all: List[Dict[str, str]] = []
+    if duplicates_by_name:
+        print('Found duplicate facilities by name (to merge):')
+        for name, duplicates in duplicates_by_name.items():
+            if not _facilities_can_be_merged(duplicates):
+                continue
+            merge_map = _get_merge_map_for_duplicates(duplicates)
+            if not merge_map:
+                continue
+            print(f'  name: {name}: {merge_map}')
+            merge_maps_all.append(merge_map)
+
+    # create final merge dictionary for duplicate facilities
+    # { id_to_merge: id_to_merge_to,... }
+    final_id_to_id_merge_map = {}
+    for merge_map in merge_maps_all:
+        for merge_from_id, merge_to_id in merge_map.items():
+            final_id_to_id_merge_map[merge_from_id] = merge_to_id
+
+    print(f'Found {len(final_id_to_id_merge_map)} facilities to merge')
+
+    if final_id_to_id_merge_map:
+        return final_id_to_id_merge_map
+
+    return None
+
+
+def handle_merge_duplicate_facilities(
+    facilities: DataFrame,
+    releases: DataFrame
+) -> DataFrame:
+    """Drops duplicate facilities by name and some other logic. Returns
+    facility DataFrame without duplicate facilities. Updates facilityId
+    values to releases DataFrame for those releases that would otherwise
+    reference to dropped (duplicate) facility.
+    """
+
+    print(f'Facilities before merging duplicates: {len(facilities)}')
+    print(f'Releases before merging duplicates: {len(releases)}')
+
+    facility_id_to_id_merge_map = _get_id_to_id_merge_map_for_facilities(
+        facilities
+    )
+    if not facility_id_to_id_merge_map:
+        return None
+
+    # drop duplicate facilities
+    facility_duplicate_ids_to_rm = facility_id_to_id_merge_map.keys()
+    facilities = facilities[
+        ~facilities['facilityId'].isin(facility_duplicate_ids_to_rm)
+    ]
+    print(f'Facilities after dropping duplicates: {len(facilities)}')
+
+    # update facilityId for releases that are linked to duplicate
+    # (dropped) facilities (use unchanged facilityId as default)
+    releases['facilityId'] = [
+        facility_id_to_id_merge_map.get(
+            r_facility_id, r_facility_id
+        )
+        for r_facility_id
+        in releases['facilityId']
+    ]
+
+    return facilities
--- api/data_import_main.py
+++ api/data_import_main.py
@@ -11,8 +11,9 @@ Engine 2016 Redistributable" (or newer)
 """
 
 from data_import.utils import (
-    add_projected_x_y_columns, print_main_activity_codes_as_enum,
-    print_unique_values_as_enum, clean_id, validate_ids
+    add_projected_x_y_columns, ensure_no_unlinked_releases,
+    handle_merge_duplicate_facilities, print_main_activity_codes_as_enum,
+    print_unique_values_as_enum, clean_id, ensure_unique_ids
 )
 from data_import.conf import conf
 import os
@@ -56,6 +57,8 @@ sql_facilities = (
     [2_ProductionFacility].countryCode,
     Max([2a_ProductionFacilityDetails].reportingYear) AS reportingYear,
     FIRST([2a_ProductionFacilityDetails].status) as status,
+    FIRST([2d_CompetentAuthorityEPRTR].CompetentAuthorityEPRTRId)
+        as CompetentAuthorityEPRTRId,
     FIRST([2d_CompetentAuthorityEPRTR].telephoneNo) as telephoneNo
     FROM (
         2_ProductionFacility
@@ -125,11 +128,11 @@ facilities = pd.read_sql_query(sql_facilities, conn)
 facilities['facilityId'] = [
     clean_id(id_str) for id_str in facilities['Facility_INSPIRE_ID']
 ]
-validate_ids(facilities)
 add_projected_x_y_columns(facilities)
 print(f'Read {len(facilities)} facilities from {conf.prtr_db_file_path}')
 
-print_main_activity_codes_as_enum(facilities)
+if conf.print_uniq_values_from_columns:
+    print_main_activity_codes_as_enum(facilities)
 
 
 releases = pd.read_sql_query(sql_releases, conn)
@@ -138,10 +141,20 @@ releases['facilityId'] = [
 ]
 print(f'Read {len(releases)} releases from {conf.prtr_db_file_path}')
 
-print_unique_values_as_enum(releases, 'pollutantCode')
-print_unique_values_as_enum(releases, 'pollutantName')
-print_unique_values_as_enum(releases, 'medium')
-print_unique_values_as_enum(releases, 'methodCode')
+
+# merge duplicate facilities by name
+facilities = handle_merge_duplicate_facilities(facilities, releases)
+# ensure unique facilityIds and that all realease are linked to some facility
+# (by facilityId)
+ensure_unique_ids(facilities)
+ensure_no_unlinked_releases(facilities, releases)
+
+
+if conf.print_uniq_values_from_columns:
+    print_unique_values_as_enum(releases, 'pollutantCode')
+    print_unique_values_as_enum(releases, 'pollutantName')
+    print_unique_values_as_enum(releases, 'medium')
+    print_unique_values_as_enum(releases, 'methodCode')
 
 
 if not os.path.exists(conf.csv_out_dir):
