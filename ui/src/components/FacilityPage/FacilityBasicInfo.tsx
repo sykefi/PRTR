9bd81a5009dcdd67160a35b31293b1bd5e1b4161
Joose Helle <joose.helle@gmail.com>
2021-10-14 20:13:51 +0300

    minor change

--- ui/src/components/FacilityPage/FacilityBasicInfo.tsx
+++ ui/src/components/FacilityPage/FacilityBasicInfo.tsx
@@ -105,7 +105,7 @@ export const FacilityBasicInfo = ({
             </Box>
             {!authorityInfo && (
               <Box marginTop={0.5} color="blackAlpha.800">
-                '-'
+                -
               </Box>
             )}
             {authorityInfo && (
