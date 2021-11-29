8c0d670b5d73062cbae584b681b1380c979e1fa3
Ismo Lahtinen <ismo.lahtinen@syke.fi>
2021-11-29 10:35:45 +0200

    Modified front page English text

--- ui/src/components/FrontPage/FrontPageContentEn.tsx
+++ ui/src/components/FrontPage/FrontPageContentEn.tsx
@@ -4,9 +4,9 @@ export const FrontPageContentEn = () => {
       <h3>Information</h3>
       <p>
         This web site presents information on releases to air and water from
-        industrial and agricultural facilities in Finland. The information is
-        retreived from PRTR (Pollutant Release and Transfer Register) maintained
-        by the European Environment Agency. 
+        industrial and agricultural facilities in Finland.The information is
+        retreived from PRTR (Pollutant Release and Transfer Register) maintained
+        by the European Environment Agency.
       </p>
       <p>
         Disclosure of the information to the European Comission is regulated by
@@ -23,26 +23,18 @@ export const FrontPageContentEn = () => {
         At the moment the presentation includes data since the reporting year
         2017.
       </p>
-      <h3>Search options</h3>
-      <p>You can search information using the following functions:</p>
-      <p>
-        <li>
-          <b>Facility, activity, location</b> returns information by facilities,
-          activities or location. 
-        </li>
-        <li>
-          <b>Päästöt ilmaan</b> näyttää päästötietojaan raportoineiden
-          PRTR-laitosten päästötietoja ilmaan taulukkomuodossa. 
-        </li>
-        <li>
-          <b>Wastewaters</b> filters releases to waterbodies. 
-        </li>
-        <li>
-          <b>On page Waste transfers</b>-total amounts are presented for wastes
-          transferred in the country as well as for transboundary waste
-          transfers. 
-        </li>
-      </p>
+
+      <li>
+        <b>Releases to air</b> returns air emissions from PRTR facilities in
+        table format.
+      </li>
+      <li>
+        <b>Releases to water</b> returns releases to waterbodies.
+      </li>
+      <li>
+        <b>Waste transfers</b> returns both domestic and transboundary waste
+        transfers.
+      </li>
       <p>
         Please read through the terms of  service before using
         the service.
