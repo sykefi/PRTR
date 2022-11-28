0c1c4f2ed47d50fda85c9c08271b568c7ef1f061
Daniel <daniel.haaranen@gmail.com>
2022-11-28 15:57:54 +0200

    Added functions for useURLSearchParams for retrieving multiple values of the same urlsearchparam.

--- ui/src/hooks/useURLSearchParams.tsx
+++ ui/src/hooks/useURLSearchParams.tsx
@@ -14,3 +14,14 @@ export const useURLSearchParamInt = (name: string): number | undefined => {
   const num = useURLSearchParam(name)
   return num !== undefined ? parseInt(num) : undefined
 }
+
+export const useURLSearchParamIntArray = (name: string): number[] | undefined => {
+  const nums = useURLSearchParamArray(name)
+  return nums !== undefined ? nums.map(elem => parseInt(elem,10)) : undefined
+}
+
+export const useURLSearchParamArray = <T extends string>(name: string): T[] | undefined => {
+  return (useURLSearchParams().getAll(name) || undefined) as T[] | undefined
+}
+
+
