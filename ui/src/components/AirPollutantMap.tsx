5a4767bfe7919365968f94e83717e5d1ad25b9d9
mirkosillanp <mirko.sillanpaa@gmail.com>
2023-08-14 16:34:12 +0300

    Improve design of AirPollutantMap tab

--- ui/src/components/AirPollutantMap.tsx
+++ ui/src/components/AirPollutantMap.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box } from '@chakra-ui/layout'
+import { Box, Container } from '@chakra-ui/layout'
 import { useTranslation } from 'react-i18next'
 
 const AirPollutantMap: React.FC = () => {
@@ -8,14 +8,21 @@ const AirPollutantMap: React.FC = () => {
   const source = 'air-pollutant-map/index.html?lang='+selectedLanguage
 
   return (
-    <Box width="100%" height="calc(90vh)">
-      <iframe
-        src={source}
-        title="Air Pollutant Map"
-        width="100%"
-        height="100%"
-      />
-    </Box>
+    <Container maxW="100%" p={3} centerContent>
+      <Box 
+        width="90%" 
+        height="calc(90vh)" 
+        backgroundColor='white' 
+        borderRadius="md" 
+        boxShadow="sm">
+        <iframe
+          src={source}
+          title="Air Pollutant Map"
+          width="100%"
+          height="100%"
+        />
+      </Box>
+    </Container>
   );
 }
 
