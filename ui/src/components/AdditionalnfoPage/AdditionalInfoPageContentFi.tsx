c30c294dcfe9c100de5681861882e0f14a99066b
Mirko Sillanpää <mirko.sillanpaa@syke.fi>
2026-03-09 13:59:49 +0200

    Update data, update texts ely -> lvv

--- ui/src/components/AdditionalnfoPage/AdditionalInfoPageContentFi.tsx
+++ ui/src/components/AdditionalnfoPage/AdditionalInfoPageContentFi.tsx
@@ -175,8 +175,8 @@ export const AdditionalInfoPageContentFi = () => {
         Suomen ympäristökeskus on toteuttanut sivuston ympäristöministeriön
         toimeksiannosta. Palautetta päästötiedoista ja muista sisältöä
         koskevista asioista voi lähettää valvontaviranomaisille (
-        <a href="https://www.ely-keskus.fi/ympariston-tilan-seuranta">
-          ELY-keskukset
+        <a href="https://lvv.fi/ymparisto/" target="blank">
+          Lupa- ja valvontavirasto
         </a>
         ). Sivuston teknisestä toiminnasta voi lähettää palautetta tai
         kehitysehdotuksia osoitteeseen{' '}
--- ui/src/components/AdditionalnfoPage/AdditionalInfoPageContentSv.tsx
+++ ui/src/components/AdditionalnfoPage/AdditionalInfoPageContentSv.tsx
@@ -174,8 +174,8 @@ export const AdditionalInfoPageContentSv = () => {
         Finlands miljöcentral har förverkligat webbsidan enligt uppdrag av
         miljöministeriet. Respons om uppgifterna och annat innehåll kan skickas
         till  övervakningsmyndigheter (
-        <a href="https://www.ely-keskus.fi/ympariston-tilan-seuranta">
-          ELY-keskukset
+        <a href="https://lvv.fi/sv/miljo" target="blank">
+          Tillstånds -och tillsynsverket
         </a>
         ). Respons om sidornas tekniska funktioner eller förbättringsförslag kan
         skickas till <a href="mailto:PRTR@syke.fi">PRTR@syke.fi</a>.
